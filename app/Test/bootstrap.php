f5c354d27aac58ae18b0729bc74f5236236f2e19
Tetsuya Takahashi <takahashi@dreamthread.net>
2015-05-25 17:23:00 +0000

    p265 ステップを実装した。フィーチャーテストも条件付きでパスした。[条件]Bddがtest_blogではなくblogデータベースを使ってしまうため、環境定義"development.php"を強引にtest_blogに繋ぎに行かせた

--- app/Test/bootstrap.php
+++ app/Test/bootstrap.php
@@ -5,3 +5,24 @@
  * Date: 2015/05/25
  * Time: 17:21
  */
+App::uses('Fabricate', 'Fabricate.Lib');
+
+Fabricate::config(function($config) {
+    $config->auto_validate = true;
+});
+/*
+Fabricate::define('Post', function($data, $world) {
+    return ['title' => $world->sequence('title', function($i) {
+        return "タイトル{$i}";
+    })];
+});
+
+$faker = Faker\Factory::create('ja_JP');
+Fabricate::define('Person', function($data, $world) use ($faker) {
+    return [
+        'name' => $faker->name,
+        'postcode' => $faker->postcode,
+        'adress' => $faker->adress,
+    ];
+});
+*/
---
+++ features/steps/blog_new_post_step.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: takahashi
+ * Date: 2015/05/25
+ * Time: 21:29
+ */
