a021d827b0ad013d169138baf80941b04d765671
Tetsuya Takahashi <takahashi@dreamthread.net>
2015-05-26 16:13:06 +0900

    テストを通過させるため、Develop環境でも test_blog を見るように変更した。原因を突き止めたら元に戻す予定

--- app/Config/bootstrap/environments/development.php
+++ app/Config/bootstrap/environments/development.php
@@ -4,7 +4,7 @@ Environment::configure('development', true, [
     'MYSQL_DB_HOST' => 'localhost',
     'MYSQL_USERNAME' => 'webapp',
 	'MYSQL_PASSWORD' => 'passw0rd',
-    'MYSQL_DB_NAME' => 'blog',
+    'MYSQL_DB_NAME' => 'test_blog',
     'MYSQL_TEST_DB_NAME' => 'test_blog',
     'MYSQL_PREFIX' => '',
 ], function() {
